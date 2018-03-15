c6f853c37df7971fefac17d676e46b0d8427cfc9
CoraYue <18772328082@163.com>
2018-03-15 19:49:43 +0800

    区域模块分页显示

--- bos_management/bos_management_service/src/main/java/com/huying/bos/service/base/AreaService.java
+++ bos_management/bos_management_service/src/main/java/com/huying/bos/service/base/AreaService.java
@@ -2,10 +2,15 @@ package com.huying.bos.service.base;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.huying.bos.domain.base.Area;
 
 public interface AreaService {
 
 	void save(List<Area> list);
+
+	Page<Area> pageQuery(Pageable pageable);
 
 }
--- bos_management/bos_management_web/src/main/java/com/huying/bos/web/action/base/AreaAction.java
+++ bos_management/bos_management_web/src/main/java/com/huying/bos/web/action/base/AreaAction.java
@@ -4,24 +4,36 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 
 import com.huying.bos.domain.base.Area;
+import com.huying.bos.domain.base.Standard;
 import com.huying.bos.service.base.AreaService;
 import com.huying.utils.PinYin4jUtils;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
+
+import net.sf.json.JSONObject;
+import net.sf.json.JsonConfig;
 
 @Namespace("/") 
 @ParentPackage("struts-default") 
@@ -105,6 +117,45 @@ public class AreaAction extends ActionSupport implements ModelDriven<Area>{
 			e.printStackTrace();
 		}
 		return SUCCESS ;
+	}
+	
+	private int page;
+	private int rows;
+	public void setPage(int page) {
+		this.page = page;
+	}
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+	@Action("areaAction_pageQuery")
+	public String pageQuery() throws IOException {
+		
+		//封装分页查询的条件
+		Pageable pageable=new PageRequest(page - 1, rows);
+		//进行分页查询
+		Page<Area> page= areaService.pageQuery(pageable);
+		//获取总Area数据条数
+		long totalElements = page.getTotalElements();
+		//获取当前页面要显示的数据
+		List<Area> list = page.getContent();
+		//由于目标页面需要的数据并不是page对象,素以需要手动封装json数据
+		Map<String, Object> map=new HashMap<>();
+		map.put("total", totalElements);
+		map.put("rows", list);
+		
+		//忽略字段
+		JsonConfig config=new JsonConfig();
+		config.setExcludes(new String[] {"subareas"});
+		//生成json数据
+		String json = JSONObject.fromObject(map,config).toString();
+		
+		//设置输出内容类型
+		HttpServletResponse response = ServletActionContext.getResponse();
+		
+		response.setContentType("application/json;charset=UTF-8");
+		//写出内容
+		response.getWriter().write(json);
+		return NONE;
 	}
 
 }
